fd970231fd2057eb82e30c08fe9ad6da0554b337
saritha-91 <saritha.kalluri91@gmail.com>
2024-05-17 15:13:19 +0100

    terraform

---
+++ conditions/variables.tf
@@ -0,0 +1,13 @@
+variable "image_id" {
+  type        = string
+  default = "ami-090252cbe067a9e58" 
+  description = "RHEL-9 AMI ID"
+}
+  variable "instance_type" {
+   default = "t3.micro"
+    type  = string
+  }
+
+  variable "instance_name" {
+      default = "db"
+  }  
